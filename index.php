73d3bbd374adbf4cbd4748dd8da3af86b4a88f41
Artur Konevnikov <artik292@gmail.com>
2018-06-10 14:33:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,26 @@
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Загрузить фотографии');
 $app->initLayout('Centered');
+
+use League\Flysystem\Filesystem;
+use League\Flysystem\Adapter\Local;
+
+$app->dbConnect('mysql://root:root@localhost/atk4');
+$adapter = new Local(__DIR__.'/localfiles');
+$app->filesystem = new Filesystem($adapter);
+class Photo extends \atk4\data\Model {
+    public $table = 'photos';
+    function init() {
+        parent::init();
+        $this->addField('name'); // friend's name
+        $this->addField('file', new \atk4\filestore\Field\File($this->app->filesystem));
+        $this->addField('file2', new \atk4\filestore\Field\File($this->app->filesystem));
+    }
+}
+$col = $app->add('Columns');
+$form = $col->addColumn()->add('Form');
+$form->setModel(new Photo($db));
+$form->model->tryLoad(1);
+$gr = $col->addColumn()->add(['Grid', 'menu'=>false, 'paginator'=>false]);
+$gr->setModel(new \atk4\filestore\Model\File($db));
+$col->js(true, new \atk4\ui\jsExpression('setInterval(function() { []; }, 2000)', [$gr->jsReload()]));
