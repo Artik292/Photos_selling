818b284e84a38bfbaafda731e04863dc1a97cbcc
Artur Konevnikov <artik292@gmail.com>
2018-06-13 14:34:06 +0300

    Update main.php

--- main.php
+++ main.php
@@ -14,10 +14,10 @@ $marge_bottom = 10;
 $sx = imagesx($stamp);
 $sy = imagesy($stamp);
 
-imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
+$res = imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
 
-header('Content-type: image/png');
-imagepng($im);
-imagedestroy($im);
+//header('Content-type: image/png');
+//imagepng($im);
+//imagedestroy($im);
 
 $app->add(["Image",$res]);
