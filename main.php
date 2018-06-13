b5277650e1e80e0e72aeb97d96923cd0ae012b36
Artur Konevnikov <artik292@gmail.com>
2018-06-13 10:34:20 +0300

    Update main.php
    Try to make watermark

--- main.php
+++ main.php
@@ -2,4 +2,19 @@
 require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Photos');
 $app->initLayout('Centered');
-$app->add[('Card','Hello word')];
+//$app->add[('Card','Hello word')];
+$stamp = imagecreatefrompng('http://pluspng.com/img-png/grass_PNG4930.png');
+$im = imagecreatefromjpeg('https://envato-shoebox-0.imgix.net/2a41/93b3-6f8b-4f1c-8767-cd9772b4ded7/kave+310.jpg?w=500&h=278&fit=crop&crop=edges&auto=compress%2Cformat&s=fbc0d75299d7cfda0b3c60ea52ba4aaf');
+
+
+$marge_right = 1;
+$marge_bottom = 1;
+$sx = imagesx($stamp);
+$sy = imagesy($stamp);
+
+
+imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
+
+header('Content-type: image/png');
+imagepng($im);
+imagedestroy($im);
