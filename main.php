339945549156b006516072cf849401f12d560bc9
Artur Konevnikov <artik292@gmail.com>
2018-06-13 10:52:39 +0300

    Update main.php

--- main.php
+++ main.php
@@ -3,9 +3,10 @@
 $app = new \atk4\ui\App('Photos');
 $app->initLayout('Centered');*/
 //$app->add[('Card','Hello word')];
-$stamp = imagecreatefrompng('https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png');
+//$stamp = imagecreatefrompng('https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png');
 //$im = imagecreatefromjpeg('https://envato-shoebox-0.imgix.net/2a41/93b3-6f8b-4f1c-8767-cd9772b4ded7/kave+310.jpg?w=500&h=278&fit=crop&crop=edges&auto=compress%2Cformat&s=fbc0d75299d7cfda0b3c60ea52ba4aaf');
-echo 'work';
+//echo 'work';
+phpinfo();
 
 /*$marge_right = 1;
 $marge_bottom = 1;
