fe98eba81a4f263fd97dfb8ea5ef04a08e68a28b
Artur Konevnikov <artik292@gmail.com>
2018-06-13 19:54:38 +0300

    Update main.php

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <?php
-/*require 'vendor/autoload.php';
+require 'vendor/autoload.php';
 $app = new \atk4\ui\App('Photos');
-$app->initLayout('Centered');*/
+$app->initLayout('Centered');
 //$app->add[('Card','Hello word')];
 $stamp = ('https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png');
 $im =('https://upload.wikimedia.org/wikipedia/commons/7/72/Pleiades_Spitzer_big.jpg');
