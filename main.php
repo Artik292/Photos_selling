17e7a93001b0b8f585efe4eca43c876ae4cc6d61
Artur Konevnikov <artik292@gmail.com>
2018-06-13 13:44:58 +0300

    Update main.php

--- main.php
+++ main.php
@@ -6,18 +6,20 @@ $app->initLayout('Centered');
 $stamp = ('https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png');
 $im =('https://envato-shoebox-0.imgix.net/2a41/93b3-6f8b-4f1c-8767-cd9772b4ded7/kave+310.jpg?w=500&h=278&fit=crop&crop=edges&auto=compress%2Cformat&s=fbc0d75299d7cfda0b3c60ea52ba4aaf');
 echo 'work';
-echo $im::h;
+var_dump ($im);
 //$app->add(["Image",$stamp]);
 //phpinfo();
 
 $marge_right = 1;
 $marge_bottom = 1;
-/*$sx = imagesx($stamp);
+$sx = imagesx($stamp);
 $sy = imagesy($stamp);
 
 
-imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
+$res = imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
 
 header('Content-type: image/png');
 imagepng($im);
-imagedestroy($im);*/
+imagedestroy($im);
+
+$app->add(["Image",$res]);
