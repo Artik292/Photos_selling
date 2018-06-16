47923f3d397cb8d032d399a1df0e7b19803a5d41
Artur Konevnikov <artik292@gmail.com>
2018-06-16 13:55:57 +0300

    Update main.php

--- main.php
+++ main.php
@@ -17,7 +17,7 @@ $sy = imagesy($stamp);
 $pleasework = imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
 
 header('Content-type: image/png');
-imagepng($im);
+//imagepng($im);
 /*$app->add(["Image",$im,"large"]);
 $app->add(["Image",$stamp,"large"]);
 $app->add(["Image",$pleasework,"large"]);*/
