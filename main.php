3251b55c75e89df50e4d34a28c28c4550321ae5a
Artur Konevnikov <artik292@gmail.com>
2018-06-16 14:56:27 +0300

    Update main.php

--- main.php
+++ main.php
@@ -1,7 +1,13 @@
 <?php
 require 'vendor/autoload.php';
+session_start();
 $app = new \atk4\ui\App('Photos');
 $app->initLayout('Centered');
 $form = $app->add(["Form"]);
 $form->addField("PNG");
 $form->addField("JPEG");
+$form->onSubmit(function($form) {
+	$_SESSION["PNG"] = $form->model("PNG");
+  $_SESSION["JPEG"] = $form->model("JPEG");
+	return new \atk4\ui\jsExpression('document.location = "watermark.php" ');
+});
