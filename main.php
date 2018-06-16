0e9ab442e19f3a97782938102fd21bd6e9b0600b
Artur Konevnikov <artik292@gmail.com>
2018-06-16 15:08:12 +0300

    Update main.php

--- main.php
+++ main.php
@@ -7,7 +7,7 @@ $form = $app->add(["Form"]);
 $form->addField("PNG");
 $form->addField("JPEG");
 $form->onSubmit(function($form) {
-	$_SESSION["PNG"] = $form["PNG"];
-  $_SESSION["JPEG"] = $form["JPEG"];
+	$_SESSION["PNG"] = $form->model["PNG"];
+  $_SESSION["JPEG"] = $form->model["JPEG"];
 	return new \atk4\ui\jsExpression('document.location = "watermark.php" ');
 });
