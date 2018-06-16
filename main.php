603efcd18a83f45155b879310e1cb04dc913c865
Artur Konevnikov <artik292@gmail.com>
2018-06-16 14:04:07 +0300

    Update main.php

--- main.php
+++ main.php
@@ -5,11 +5,12 @@ $app->initLayout('Centered');
 //$app->add[('Card','Hello word')];
 $stamp = imagecreatefrompng('https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png');
 $im = imagecreatefromjpeg('https://upload.wikimedia.org/wikipedia/commons/7/72/Pleiades_Spitzer_big.jpg');
+$app->add(["Image",$im]);
 //echo 'work';
 //var_dump ($im);
 //$app->add(["Image",$stamp]);
 //phpinfo();
-$marge_right = 1;
+/*$marge_right = 1;
 $marge_bottom = 1;
 $sx = imagesx($stamp);
 $sy = imagesy($stamp);
@@ -17,7 +18,7 @@ $sy = imagesy($stamp);
 $pleasework = imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
 
 header('Content-type: image/png');
-imagepng($im);
+imagepng($im); */
 /*$app->add(["Image",$im,"large"]);
 $app->add(["Image",$stamp,"large"]);
 $app->add(["Image",$pleasework,"large"]);*/
