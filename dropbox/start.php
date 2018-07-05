3a9cb8d77e736b9be1a749d7f598f0646f1d5397
artik292 <artik292@gmail.com>
2018-07-05 15:29:27 +0300

    Connection to CLear_DB

--- dropbox/start.php
+++ dropbox/start.php
@@ -13,9 +13,11 @@ $appInfo = new Dropbox\AppInfo($dropboxKey, $dropboxSecret);
 $csrfTokenStore = new Dropbox\ArrayEntryStore($_SESSION, 'dropbox-auth-csrf-token');
 
 $webAuth = new Dropbox\WebAuth($appInfo, $appName, 'http://localhost/Photos_selling/dropbox/finish.php',$csrfTokenStore);
-
-$db = new PDO('mysql:host=localhost;dbname=photo_selling','root','');
-
+  if (isset($_ENV['CLEARDB_DATABASE_URL'])){
+      $db = new PDO($_ENV['CLEARDB_DATABASE_URL']);
+  } else {
+      $db = new PDO('mysql:host=localhost;dbname=photo_selling','root','');
+  }
 $user = $db->prepare("SELECT * FROM users WHERE id = :user_id");
 $user->execute(['user_id' => $_SESSION['user_id']]);
 $user = $user->fetchObject();
