bdcc199cc664f16dc5658d6ed3489e3d1b070c5b
Artur Konevnikov <artik292@gmail.com>
2018-07-20 12:54:55 +0300

    Index upgrade
    Clean all up.

--- shopping-basket/index.php
+++ shopping-basket/index.php
@@ -1,45 +1,36 @@
 <?php
-session_start();
+require 'con.php';
 if (!(isset($_SESSION['sum']))) {
     $_SESSION['sum'] = 0;
 }
-require 'con.php';
 
-//echo $_SESSION['sum'];
+$app = new App();
 
-$app = new \atk4\ui\App('Тест корзины');
-$app->initLayout('Centered');
+$app->add(['CRUD'])->setModel(new Photo($app->db));
 
-
-//////////////////////////////FOR CONNECTION////////////////////////////////
-
-
-
-////////////////////////////////////////////////////////////////////////
-
-$app->add(['CRUD'])->setModel(new Photo($db));
-
-If ((($_SESSION['sum']*10) % 10) == 0) {
+if ((($_SESSION['sum']*100) % 10) == 0) {
+    If ($_SESSION['sum'] == 0) {
     $Button = $app->add(['Label',$_SESSION['sum'].' €','big red right ribbon','icon'=>'shopping cart']);
-} else {
+    } else {
     $Button = $app->add(['Label',$_SESSION['sum'].'0 €','big red right ribbon','icon'=>'shopping cart']);
+    }
+} else {
+    $Button = $app->add(['Label',$_SESSION['sum'].' €','big red right ribbon','icon'=>'shopping cart']);
 }
 $Button->link(['basket']);
 
-$photo = new Photo($db);
+$photo = new Photo($app->db);
 
 $app->add(['ui'=>'hidden divider']);
 
 $page = $app->add(['View', 'id' => 'example']);
 
 $c = $page->add(new \atk4\ui\Columns());
-$c1 = $c->addColumn(); //->add(['LoremIpsum', 1]);
-$c2 = $c->addColumn(); //->add(['LoremIpsum', 1]);
-$c3 = $c->addColumn(); //->add(['LoremIpsum', 1]);
+$c1 = $c->addColumn();
+$c2 = $c->addColumn();
+$c3 = $c->addColumn();
 
 $app->add(['ui'=>'hidden divider']);
-
-//$app->add(['Card'])->setModel((new Photo($db))->tryLoadAny());
 
 $i=1;
 $image = new SimpleImage();
@@ -62,34 +53,33 @@ foreach ($photo as $p) {
         $c3->add(['ui'=>'hidden divider']);
         break;
 }
-      $image->load('photos/'.$p['photo_id'].'.jpg');
-      $image->resize(440, 333);
-      $image->save('photos/'.$p['photo_id'].'s.jpg');
-      mark('photos/'.$p['photo_id'].'s.jpg');
+      $app->mark('photos/'.$p['photo_id'].'.jpg');
       $view->add(['Image','photos/'.$p['photo_id'].'s.jpg','small']);
-      If ((($p['value']*10) % 10) == 0) {
+
+      $m = $view->add('Menu');
+      $sm = $m->addMenu('Sub-menu');
+      foreach ($rez as $r) {
+        $sm->addItem($r['rez'].' '.$r['value'])->on('click',  function() use($app,$r) {
+        $_SESSION['sum'] = $_SESSION['sum']+$r['value'];
+        $Button->set($_SESSION['sum']);
+        $app->bask_add($r['value']);
+        return new \atk4\ui\jsExpression('document.location = "index.php" ');
+      });
+      }
+      /*If ((($p['value']*10) % 10) == 0) {
           $B = $view->add(['Button',$p['value'].' €','massive green tag']);
       } else {
           $B = $view->add(['Button',$p['value'].'0 €','massive green tag']);
-      }
-      $B->on('click',  function() use($app,$Button,$p) {
+      } */
+      /*$B->on('click',  function() use($app,$Button,$p) {
       $_SESSION['sum'] = $_SESSION['sum']+$p['value'];
       $Button->set($_SESSION['sum']);
-      bask_add($p);
-      //Header ('Location index.php');
-      return new \atk4\ui\jsExpression('document.location = "index.php" ');
-    });
+      $app->bask_add($p);
+      return new \atk4\ui\jsExpression('document.location = "index.php" '); */
+    //});
 }
 
 $app->add(['Button','Reset'])->on('click', function($app) {
 $_SESSION['sum'] = 0;
 return new \atk4\ui\jsExpression('document.location = "index.php" ');
 });
-
-/*$app->add(['Button','Do watermark'])->on('click', function($app) {
-  $image = new SimpleImage();
-  $image->load('photos/w.png');
-  //$image->scale(100);         Can't do it with PNG
-  $image->save('photos/wat.png');
-return new \atk4\ui\jsExpression('document.location = "index.php" ');
-}); */
