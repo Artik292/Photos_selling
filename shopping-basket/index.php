871696f0e6e539f72c5f5a19196911dc497ab49c
Artur Konevnikov <artik292@gmail.com>
2018-07-27 13:07:25 +0300

    Update index.php

--- shopping-basket/index.php
+++ shopping-basket/index.php
@@ -18,7 +18,8 @@ $app->add(['CRUD'])->setModel(new Photo($app->db));
     $Button = $app->add(['Label',$_SESSION['sum'].' €','big red right ribbon','icon'=>'shopping cart']);
 } */
 
-$Button = $app->add(['Label',$_SESSION['sum'].' €','big red right ribbon','icon'=>'shopping cart']);
+//$Button = $app->add(['Label',$_SESSION['sum'].' €','big red right ribbon','icon'=>'shopping cart']);
+$Button = $app->add(['Button',$_SESSION['sum'].' €','big red right ribbon','icon'=>'shopping cart']);
 
 $Button->link(['basket']);
 
