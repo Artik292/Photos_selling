ac5fa249a8ec6a0259149eba605547b080db6909
artik292 <artik292@gmail.com>
2018-08-28 12:03:38 +0300

    Watermark corection

--- shopping-basket/watermark.php
+++ shopping-basket/watermark.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$stamp = imagecreatefrompng($_SESSION["PNG"]);
+$stamp = imagecreatefrompng('photos/watermark.png');
 $im = imagecreatefromjpeg($_SESSION["JPEG"]);
 //$im1 = ('https://upload.wikimedia.org/wikipedia/commons/7/72/Pleiades_Spitzer_big.jpg');
 //echo 'work';
@@ -15,7 +15,7 @@ $sy = imagesy($stamp);
 $pleasework = imagecopy($im, $stamp, imagesx($im) - $sx - $marge_right, imagesy($im) - $sy - $marge_bottom, 0, 0, imagesx($stamp), imagesy($stamp));
 
 header('Content-type: image/png');
-imagepng($im); 
+imagepng($im);
 /*$app->add(["Image",$im,"large"]);
 $app->add(["Image",$stamp,"large"]);*/
 //$app->add(["Image",$pleasework,"large"]);
